3fdb9bfb8075a4a48fe7b29434d97dde0bc86cea
tanushbanerjee <samihanbanerjee026@gmail.com>
2020-08-14 09:46:36 +0530

    Added command for adding floders based on their type

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
 import os
+
+def createNotExists(folder):
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
+
+files = os.listdir()
+print(files)
+
+createNotExists('Images')
+createNotExists('Docs')
+createNotExists('Media')
+createNotExists('Other')
+
+imgExts = [".png", ".img", ".jpg", ".jpeg", ".ico"]
+images = [files for file in files if os.path.splitext(file)[1].lower() in imgExts]
+
+docsExts = [".txt", ".docx", ".doc", ".pdf", ".odt"]
+docs = [files for file in files if os.path.splitext(file)[1].lower() in docsExts]
+
+mediaExts = [".mp4", ".mp3", ".mkv", ".flv"]
+media = [files for file in files if os.path.splitext(file)[1].lower() in mediaExts]
+
+
